96958205abbb5a6f144c945fe377a78ebd651f5d
Jeff Laake <jefflaake@gmail.com>
2015-02-11 09:21:09 -0800

    Addition of ejresight and ejcheck functions for EJ resight data entry from ACDsee metadata

--- CalcurData/R/metadata.conversion.r
+++ CalcurData/R/metadata.conversion.r
@@ -136,5 +136,150 @@ bites=function(inp,out,append=FALSE)
 	close(con_out)
 	invisible()
 }
+#' Convert resight file from ACDSee format for Ej sightings to data entry template format 
+#' 
+#' @param inp name of input file containing metadata from ACDsee; if NULL can select from file browser
+#' @param out name of output file which is formatted for data copying to data entry template
+#' @param append if TRUE, append to the output file
+#' @return no return value
+#' @export ejresights 
+#' @author Jeff Laake, Jeff Harris
+ejresights=function(inp=NULL,out,append=FALSE)
+{
+	if(is.null(inp))inp=file.choose()
+	library(stringr)
+	if(append)  
+		con_out=file(out,open="at")
+	else
+		con_out=file(out,open="wt")
+	xx=readLines(inp)
+	xnam=c("Filename","Title","Headline","Description","Address","City","Country","Creator","Email","Job Title","IPTC Scene Code","Phone","Postal Code","State/Province",
+			"Web URL","Copyright Notice","Rights Usage Terms","Intellectual Genre","Location")   
+	ivec=sapply(xnam,function(x) regexpr(x,xx[4])[1])
+	image=gsub(" ","",substr(xx[-(1:5)],1,ivec[2]-1))
+	brand=gsub(" ","",(substr(xx[-(1:5)],ivec[2],ivec[3]-1)))
+	datetime=substr(xx[-(1:5)],ivec[3],ivec[4]-1)
+	date=sapply(strsplit(datetime," "),function(x)x[1])
+	datemat=do.call("rbind",strsplit(date,"/"))
+	region=gsub(" ","",substr(xx[-(1:5)],ivec[5],ivec[6]-1))
+	sitename=gsub(" ","",substr(xx[-(1:5)],ivec[6],ivec[7]-1))
+	condition=gsub(" ","",substr(xx[-(1:5)],ivec[7],ivec[8]-1))
+	condition[condition=="QUALITY"&nchar(brand)<=4]=sapply(nchar(brand),function(x) paste(rep("0",x),collapse=""))[condition=="QUALITY"&nchar(brand)<=4]
+	condition=gsub(" ","",condition)
+	condition=gsub("'","",condition)
+#	condition=paste("'",condition,sep="")
+	condition[condition=="QUALITY"]=""
+	primaryobs=gsub(" ","",substr(xx[-(1:5)],ivec[8],ivec[9]-1))
+	leftstatus=gsub(" ","",substr(xx[-(1:5)],ivec[9],ivec[10]-1))
+	rightstatus=gsub(" ","",substr(xx[-(1:5)],ivec[15],ivec[16]-1))
+	secondaryobs=gsub(" ","",substr(xx[-(1:5)],ivec[10],ivec[11]-1))
+	comments=str_trim(substr(xx[-(1:5)],ivec[11],ivec[12]-1))
+	photo=rep("YES",length(brand))
+	tagnumber=gsub(" ","",substr(xx[-(1:5)],ivec[12],ivec[13]-1))
+	check=gsub(" ","",substr(xx[-(1:5)],ivec[13],ivec[14]-1))
+	platform=gsub(" ","",substr(xx[-(1:5)],ivec[14],ivec[15]-1))
+	age=gsub(" ","",substr(xx[-(1:5)],ivec[16],ivec[17]-1))
+	sex=gsub(" ","",substr(xx[-(1:5)],ivec[17],ivec[18]-1))
+	rbi=gsub(" ","",substr(xx[-(1:5)],ivec[18],ivec[19]-1))
+	sel=brand!="BRAND/TAGID"
+	bl=substr(brand,nchar(brand),nchar(brand))
+	bl2=substr(brand,1,1)
+	bl.last=suppressWarnings(is.na(as.numeric(bl)))
+	bl.first=suppressWarnings(is.na(as.numeric(bl2)))
+	BrandLetter=bl
+	BrandLetter[bl.first]=bl2[bl.first]
+	BrandNumber=substr(brand,1,nchar(brand)-1)
+	BrandNumber[bl.first]=substr(brand[bl.first],2,nchar(brand[bl.first]))
+	BrandNumber=suppressWarnings(as.numeric(BrandNumber))
+	
+	df=cbind(BrandLetter=BrandLetter,BrandNumber=BrandNumber,Brand=brand,BrandLocation="L",BrandQuality=condition,Check=check,TagNumber=tagnumber,LTag=leftstatus,RTag=rightstatus,
+			TagColor="W",Month=as.numeric(datemat[,2]),Day=as.numeric(datemat[,3]),Year=as.numeric(datemat[,1]),Time="",Sex=sex,Age=age,
+			Region=region,SiteName=sitename,SubSite="",RBI=rbi,Obs=primaryobs,Obs2=secondaryobs,Platform=platform,photo=photo,Comments=comments,image=image,DateEntered=format(Sys.time()),Radio="",Patch="")
+	
+	df=df[sel,]
+	
+	if(append)
+		write.table(df,con_out,row.names=FALSE,col.names=FALSE,sep="\t")
+	else
+		write.table(df,con_out,row.names=FALSE,sep="\t")
+	close(con_out)
+	invisible()
+}
+#' Validates Ej resight data  
+#' 
+#' @param filename name of file output from ejresights function; if NULL can select with file browser
+#' @return no return value
+#' @export ejcheck
+#' @author Jeff Laake, Jeff Harris
+
+ejcheck=function(filename=NULL)
+{
+	if(is.null(filename))filename=file.choose()    
+	con=file(filename,open="rt")
+	df=read.table(con,sep="\t",header=TRUE,stringsAsFactors=FALSE,colClasses="character")
+#	colC=sapply(df,mode)
+#    colC[colC=="logical"]="character"
+#	con1=file(filename,open="rt")
+#	df=read.table(con1,sep="\t",header=TRUE,stringsAsFactors=FALSE,colClasses=colC)
+	tblSex=getCalcurData("Ej",tbl="tblSex")
+	sex_values=as.character(tblSex$Sex)
+	tblSitenames=getCalcurData("Ej",tbl="tblSitenames")
+	site_values=as.character(tblSitenames$sitename)
+	tblRegions=getCalcurData("Ej",tbl="tblRegions")
+	region_values=as.character(tblRegions$Region)
+	tblRBI=getCalcurData("Ej",tbl="tblRBI")
+	rbi_values=as.character(tblRBI$RBI)
+	tblObs=getCalcurData("Ej",tbl="tblObs")
+	obs_values=as.character(tblObs$Obs)
+	tblTag=getCalcurData("Ej",tbl="tblLRTag")	
+	tag_values=as.character(tblTag$LRTag)
+	tblcheck=getCalcurData("Ej",tbl="tblBrandCheck")	
+	check_values=as.character(tblcheck$BrandCheck)
+	tblage=getCalcurData("Ej",tbl="tblAge")	
+	age_values=as.character(tblage$Age)
+	tblBrandLetter=getCalcurData("Ej",tbl="tblBrandLetter")	
+	brandletter_values=as.character(tblBrandLetter$BrandLetter)
+	tblplatform=getCalcurData("Ej",tbl="tblPlatform")	
+	platform_values=as.character(tblplatform$Platform)
+	tblbrand= getCalcurData("Ej",tbl="tblBrands")  
+	brand_values=as.character(tblbrand$Brand)
+	# loop through records and test
+	for( i in 1:nrow(df))
+	{
+		cat("\nRecord ",i,":")
+		ok=TRUE
+		test=function(field,values)
+		{
+			if(!df[i,field]%in%values)
+			{
+				cat("\nBad value for ",field," = ",as.character(df[i,field]))
+				return(FALSE)
+			} else
+				return(TRUE)
+		}
+		ok=test("Sex",sex_values)
+		ok=ok&test("Platform",platform_values)
+		ok=ok&test("SiteName",site_values)
+		ok=ok&test("Region",region_values)
+		ok=ok&test("RBI",rbi_values)
+		ok=ok&test("Obs",obs_values)
+		ok=ok&test("Obs2",obs_values)
+		ok=ok&test("LTag",tag_values)
+		ok=ok&test("RTag",tag_values)
+		ok=ok&test("Check",check_values)
+		ok=ok&test("Age",age_values)
+		ok=ok&test("BrandLetter",brandletter_values)
+		ok=ok&test("Brand",brand_values)
+		ok=ok&test("Month",as.character(1:12))
+		ok=ok&test("Day",as.character(1:31))
+		if(!(as.numeric(as.character(df[i,"Year"]))>2000))
+		{
+			cat("\nBad value for Year = ",as.character(df[i,"Year"]))
+			ok=FALSE
+		}
+		if(ok) cat("ok\n")
+	}
+	invisible()
+}
 
 
